6cd5589596c24a8f8e77e8ba76e45c1cb9c54608
HasnainAbass <hasnainabass243@gmail.com>
2024-03-25 11:09:49 +0500

    initial files!

---
+++ a7has-cli-num-gessing-game/index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const rendumNum = Math.floor(Math.random() * 6 + 1); 
+const userNum = await inquirer.prompt([
+    {message:"Please guess a Game Number between ( 1 to 6 )!",
+    type:"number",
+    name:"userGuessedNumber",
+     },
+]);
+
+if(userNum.userGuessedNumber === rendumNum){
+console.log("conguratulation , you Guessed a Right number!");
+
+}else{
+console.log("You Guess Wrong number please again!");
+
+}
+
+
+
